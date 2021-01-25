1845b1af36cb03018847d3490cb2188c12f10641
manbakescode <42654011+manbakescode@users.noreply.github.com>
2021-01-25 00:31:34 +0000

    Shuaib: spring-boot app initializer
    initilized the spring-boot app.

---
+++ recycling-app/src/main/java/com/group20/recyclingapp/NoWasteApplication.java
@@ -0,0 +1,13 @@
+package com.group20.recyclingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoWasteApplication.class, args);
+	}
+
+}
---
+++ recycling-app/src/test/java/com/group20/recyclingapp/NoWasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group20.recyclingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
