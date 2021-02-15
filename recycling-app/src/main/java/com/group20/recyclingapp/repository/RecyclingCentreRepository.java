6a5a2e37f73193d081f5a67d381375034fa41e13
manbakescode <42654011+manbakescode@users.noreply.github.com>
2021-02-15 23:23:55 +0000

    Created RecyclingCentre & Location Repository

---
+++ recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclingCentreRepository.java
@@ -0,0 +1,8 @@
+package com.group20.recyclingapp.repository;
+
+import com.group20.recyclingapp.model.RecyclingCentre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecyclingCentreRepository extends JpaRepository <RecyclingCentre, Long>{
+
+}
