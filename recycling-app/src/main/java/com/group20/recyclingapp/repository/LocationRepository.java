9e8dc9e226c8d8023c670e8c7d44e66443621a81
manbakescode <42654011+manbakescode@users.noreply.github.com>
2021-02-18 23:53:21 +0000

    added Repository annotation

--- recycling-app/src/main/java/com/group20/recyclingapp/repository/LocationRepository.java
+++ recycling-app/src/main/java/com/group20/recyclingapp/repository/LocationRepository.java
@@ -2,6 +2,8 @@ package com.group20.recyclingapp.repository;
 
 import com.group20.recyclingapp.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends JpaRepository <Location, Long> {
 }
--- recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclingCentreRepository.java
+++ recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclingCentreRepository.java
@@ -2,7 +2,9 @@ package com.group20.recyclingapp.repository;
 
 import com.group20.recyclingapp.model.RecyclingCentre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecyclingCentreRepository extends JpaRepository <RecyclingCentre, Long>{
 
 }
