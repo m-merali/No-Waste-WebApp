1b501bb5b48edcd176fd97c2876cafeb2d5d0a62
manbakescode <42654011+manbakescode@users.noreply.github.com>
2021-02-20 23:58:10 +0000

    findAllbyLocation added

--- recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclingCentreRepository.java
+++ recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclingCentreRepository.java
@@ -1,10 +1,11 @@
 package com.group20.recyclingapp.repository;
 
+import com.group20.recyclingapp.model.Location;
 import com.group20.recyclingapp.model.RecyclingCentre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecyclingCentreRepository extends JpaRepository <RecyclingCentre, Long>{
-
+    RecyclingCentre findAllByLocation(Location location);
 }
