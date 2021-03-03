fa835668b53af93c9a842bc97a0f4366481d90de
ZakriaR <zakriar99@gmail.com>
2021-03-03 12:09:35 +0000

    Revert "Made homepage compatible with thymeleaf"
    This reverts commit 60f6c8c172b15037aa2ab1f7da33b2fa9801d458.

--- recycling-app/src/main/resources/static/js/script.js
+++
@@ -1,6 +0,0 @@
-let content = document.getElementsByTagName('body')[0];
-let darkMode = document.getElementById('dark-mode');
-darkMode.addEventListener('click', function () {
-    darkMode.classList.toggle('active');
-    content.classList.toggle('night');
-})
