eed06b7d0fccadaf40d4524e5a793575525b0764
Bara Konateh <bkonateh@gmail.com>
2021-03-10 04:37:02 +0000

    Integrated Backend/halfway

--- recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclableRepository.java
+++ recycling-app/src/main/java/com/group20/recyclingapp/repository/RecyclableRepository.java
@@ -1,5 +1,12 @@
 package com.group20.recyclingapp.repository;
 
-public class RecyclableRepository extends CrudRepository<>{
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.group20.recyclingapp.model.Items;
+
+@Repository
+public interface RecyclableRepository extends JpaRepository <Items, Integer> {
+
+    @Query(value = "SELECT * FROM items WHERE MATCH(item_name, item_description AGAINST (?1)", nativeQuery = true) //query to search database for related items
+        public List<Items> search(String keyword);
 }
